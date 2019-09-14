a403f39b1580f4c9ba5e73f051de987f2ce2f980
Erkanovich <erinik90@gmail.com>
2019-09-14 22:59:49 +0200

    added functionality for user-create form

--- src/components/user/user-create.js
+++ src/components/user/user-create.js
@@ -6,44 +6,57 @@ import FormLabel from "react-bootstrap/FormLabel";
 import FormCheck from "react-bootstrap/FormCheck";
 
 export default class UserCreate extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      firstName: '',
+      lastName: '',
+      dateOfBirth: undefined,
+      sex: '',
+      phone: '',
+      department: ''
+  };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleSubmit(event){
+    event.preventDefault();
+    console.log('SKICKADE', this.state);
+  }
+
+  formGroup(valueKey, labelText = "", type = "text") {
+    return (
+      <FormGroup controlId={valueKey}>
+        <FormLabel>{labelText}</FormLabel>
+        <FormControl onChange={e => this.setState({[`${valueKey}`]: e.target.value})} type={type} value={this.state[valueKey]}></FormControl>
+      </FormGroup>
+    );
+  }
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.handleSubmit}>
         <h3>Skapa profil</h3>
-        <FormGroup>
-          <FormLabel>Välj lösenord</FormLabel>
-          <FormControl type="password"></FormControl>
+        {this.formGroup('email', 'Email')}
+        {this.formGroup('password', 'Välj lösenord', 'password')}
+        {this.formGroup('confirmPassword', 'Bekräfta lösenord', 'password')}
+        {this.formGroup('firstName', 'Förnamn')}
+        {this.formGroup('lastName', 'Efternamn')}
+        {this.formGroup('dateOfBirth', 'Födelsedatum', 'date')}
+        <FormGroup onChange={e => this.setState({sex: e.target.value})}>
+          <FormLabel>Kön</FormLabel>
+          <FormCheck id="male" name="sex_radios" type="radio" label="Man" value="MALE"></FormCheck>
+          <FormCheck id="female" name="sex_radios" type="radio" label="Kvinna" value="FEMALE"></FormCheck>
+          <FormCheck id="other" name="sex_radios" type="radio" label="Annat" value="OTHER"></FormCheck>
         </FormGroup>
-        <FormGroup>
-          <FormLabel>Bekräfta lösenord</FormLabel>
-          <FormControl type="password"></FormControl>
-        </FormGroup>
-        <FormGroup>
-          <FormLabel>Förnamn</FormLabel>
-          <FormControl type="text"></FormControl>
-        </FormGroup>
-        <FormGroup>
-          <FormLabel>Efternamn</FormLabel>
-          <FormControl type="text"></FormControl>
-        </FormGroup>
-        <FormGroup>
-          <FormLabel>Födelsedatum</FormLabel>
-          <FormControl type="date"></FormControl>
-        </FormGroup>
-        <FormGroup>
-          <FormLabel controlId="sexRadio">Kön</FormLabel>
-          <FormCheck type="radio" label="Man"></FormCheck>
-          <FormCheck type="radio" label="Kvinna"></FormCheck>
-          <FormCheck type="radio" label="Annat"></FormCheck>
-        </FormGroup>
-        <FormGroup>
-          <FormLabel>Telefonnummer</FormLabel>
-          <FormControl type="text"></FormControl>
-        </FormGroup>
-        <FormGroup>
-          <FormLabel>Avdelning jag är chef för:</FormLabel>
-          <FormControl type="text"></FormControl>
-        </FormGroup>
+        {this.formGroup('phone', 'Telefonnummer')}
+        {this.formGroup('department', 'Avdelning jag är chef för')}
         <FormGroup>
           <FormControl type="submit" value="Fortsätt"></FormControl>
         </FormGroup>
